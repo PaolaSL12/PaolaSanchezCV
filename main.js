eff80ce002a1faf00fcb20478860222c1acf6f8e
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-19 20:19:26 +0100

    fixing hero

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import { cerateHero } from './src/components/Hero/hero'
 import { createNavBar } from './src/components/NavBar/NavBar'
 import './style.css'
 
-createNavBar()
-cerateHero()
+createNavBar();
+cerateHero();
