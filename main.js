e50ac322cb9911991ff7db69bf179ea66186399d
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-19 21:28:17 +0100

    fixing thin

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { cerateHero } from "./src/components/Hero/hero.js";
 import { createNavBar } from "./src/components/NavBar/NavBar.js";
-
+import { data } from "./src/components/data/data.js";
+data
 
 
 createNavBar();
