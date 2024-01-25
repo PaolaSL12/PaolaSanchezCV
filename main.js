cc5c08a28c305571a8f4ca9a0a1a5f73ff2f7106
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-25 18:27:14 +0100

    finishing main.js

--- main.js
+++ main.js
@@ -12,4 +12,5 @@ createNavBar();
 createHero();
 buttons();
 createSectionDiv();
+createWorkExpirience();
 createProyects();
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -4,5 +4,7 @@ export const createSectionDiv = () => {
     const sectionDiv = document.createElement("div");
     sectionDiv.className = "sectionDiv";
 
+    
+
     document.querySelector("#app").append(sectionDiv);
 }
