a3e847d6ff378019abcd0b5ff55c5cee6764df52
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-25 18:21:00 +0100

    work details

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { buttons } from "./src/components/Buttons/Buttons.js";
 import { createHero } from "./src/components/Hero/Hero.js";
 import { createNavBar } from "./src/components/NavBar/NavBar.js";
 import { createProyects } from "./src/components/Proyects/Proyects.js";
+import { createWorkExpirience } from "./src/components/WorkExpirience/WorkExpirience.js";
 import { createSectionDiv } from "./src/components/section/section.js";
 
 import './style.css'
@@ -9,6 +10,7 @@ import './style.css'
 
 createNavBar();
 createHero();
-buttons()
+buttons();
+createWorkExpirience();
 createSectionDiv();
 createProyects();
